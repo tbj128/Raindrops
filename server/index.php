c29dd5877b5ac42306b9fc63780f5e0c8555ad96
Tom Jin <tbj128@yahoo.ca>
2014-12-24 23:56:41 -0800

    Updated setup redirect

--- server/index.php
+++ server/index.php
@@ -11,8 +11,7 @@
 
 	$mysqli = new mysqli($db_host, $db_username, $db_password, $db_database);
 	if (mysqli_connect_errno()) {
-		printf("Connect failed: %s\n", mysqli_connect_error());
-		exit();
+		header("Location: setup.php");
 	}
 
 	sec_session_start();
